7eb5aade7f38fc2531580a4e71572777a7ee0a07
IreneBioque <irenebioque@gmail.com>
2022-09-26 19:14:35 +0200

    add seed, config and add methods of controllers

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v2');
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,7 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
 
 export class CreatePokemonDto {
-  no: number;
   @IsString()
+  @MinLength(1)
   name: string;
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
 }
